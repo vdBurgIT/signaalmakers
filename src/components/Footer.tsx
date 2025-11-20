388843a24a52300421658bc513f60e7c9bc92308
Kevin Froger <kevin@kevinfroger.nl>
2025-11-20 19:58:06 +0100

    adres footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -116,7 +116,7 @@ export default function Footer() {
             <ul className="space-y-3 text-gray-300">
               <li className="flex items-start gap-2">
                 <MapPin className="w-5 h-5 text-[#FF6A00] flex-shrink-0 mt-0.5" />
-                <span>Voorbeeldstraat 123<br />1234 AB Utrecht</span>
+                <span>Vezelstraat 14<br />3295 VP 's-Gravendeel</span>
               </li>
               <li className="flex items-center gap-2">
                 <Phone className="w-5 h-5 text-[#FF6A00]" />
